cce4f3bedbd937b8f91a9706e8c9c874c2efd0c8
Cloud <cloudinstone@gmail.com>
2026-05-22 00:20:18 +0800

    fix(download): use curl for proxy support and progress reporting

--- bin/cli.js
+++ bin/cli.js
@@ -275,26 +275,21 @@ function handleEnv() {
 
 function downloadFile(url, dest) {
     return new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(dest);
-        const request = https.get(url, (response) => {
-            if (response.statusCode === 301 || response.statusCode === 302) {
-                return downloadFile(response.headers.location, dest).then(resolve).catch(reject);
-            }
-            if (response.statusCode !== 200) {
-                return reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
-            }
-            response.pipe(file);
-            file.on('finish', () => {
-                file.close(resolve);
-            });
-        }).on('error', (err) => {
-            fs.unlink(dest, () => {});
-            reject(err);
+        const { spawn } = require('child_process');
+        const curl = spawn('curl', ['-L', '--fail', '-o', dest, url], {
+            stdio: 'inherit' // Show curl's native progress bar
         });
-        
-        request.setTimeout(60000, () => {
-            request.destroy();
-            reject(new Error("Download timeout"));
+
+        curl.on('close', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`curl failed with exit code ${code}`));
+            }
+        });
+
+        curl.on('error', (err) => {
+            reject(err);
         });
     });
 }
